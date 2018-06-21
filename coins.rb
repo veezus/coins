c6322e56e8881a57b04e7234185fa07146430bd6
Veezus Kreist <almighty@veez.us>
2018-06-21 11:02:42 -0500

    Add custom currency spec

--- coins.rb
+++ coins.rb
@@ -1,20 +1,25 @@
 class Coins
+  EUR_DENOMINATIONS = {
+    two_euro: 200, one_euro: 100, half_euro: 50, twenty_cent: 20, ten_cent: 10,
+    five_cent: 5, two_cent: 2, one_cent: 1,
+  }
+  USD_DENOMINATIONS = {
+    quarter: 25, dime: 10, nickel: 5, penny: 1,
+  }
+
   attr_accessor :cents, :denominations
 
-  def initialize cents:, denominations: {quarter: 25, dime: 10, nickel: 5, penny: 1}
+  def initialize cents:, currency_code:
     self.cents = cents
-    self.denominations = denominations
+    self.denominations = denominations_from_currency_code currency_code
   end
 
   def make_change
     remaining_change = cents
-    {}.tap do |result|
-      denominations.each do |denomination, amount_of_denomination|
-         change = change_in_denomination remaining_change,
-           amount_of_denomination
-         result[denomination] = change[:number_of_denomination]
-         remaining_change = change[:remaining_change]
-      end
+    denominations.each_with_object({}) do |(denomination, amount_of_denomination), result|
+      change = change_in_denomination remaining_change, amount_of_denomination
+      result[denomination] = change[:number_of_denomination]
+      remaining_change = change[:remaining_change]
     end
   end
 
@@ -27,4 +32,17 @@ class Coins
       remaining_change: remaining
     }
   end
+
+  private
+
+  def denominations_from_currency_code code
+    return EUR_DENOMINATIONS if code == 'EUR'
+    return USD_DENOMINATIONS if code == 'USD'
+
+    raise UnsupportedCurrencyError
+  end
+end
+
+class UnsupportedCurrencyError < StandardError
+
 end
--- coins_spec.rb
+++ coins_spec.rb
@@ -2,29 +2,58 @@ require 'pry'
 require 'rspec'
 require_relative 'coins'
 
-# Construct a program that will make change for a given dollar amount in cents
-# using the fewest number of coins.
-
 describe Coins do
-  describe "#make_change_for" do
+  context "#with EUR currency_code" do
+    let(:coins) do
+      Coins.new cents: 0, currency_code: 'EUR'
+    end
+
     it "returns the correct change" do
-      coins = Coins.new cents: 99
+      coins.cents = 599
       expect(coins.make_change).to eq(
-        { quarter: 3, dime: 2, nickel: 0, penny: 4 }
-      )
-      coins = Coins.new cents: 31
-      expect(coins.make_change).to eq(
-        { quarter: 1, dime: 0, nickel: 1, penny: 1 }
+        {
+          two_euro: 2,
+          one_euro: 1,
+          half_euro: 1,
+          twenty_cent: 2,
+          ten_cent: 0,
+          five_cent: 1,
+          two_cent: 2,
+          one_cent: 0,
+        }
       )
     end
   end
 
-  describe "#change_in_denomination" do
-    let(:coins) { Coins.new(cents: 99) }
-    it "knows the correct number of coins for the value" do
-      expect(coins.change_in_denomination(99, 25)).to eq(
-        { number_of_denomination: 3, remaining_change: 24 }
-      )
+  context "with USD currency_code" do
+    describe "#make_change" do
+      it "returns the correct change" do
+        coins = Coins.new cents: 99, currency_code: 'USD'
+        expect(coins.make_change).to eq(
+          { quarter: 3, dime: 2, nickel: 0, penny: 4 }
+        )
+        coins = Coins.new cents: 31, currency_code: 'USD'
+        expect(coins.make_change).to eq(
+          { quarter: 1, dime: 0, nickel: 1, penny: 1 }
+        )
+      end
+    end
+
+    describe "#change_in_denomination" do
+      let(:coins) { Coins.new cents: 99, currency_code: 'USD' }
+      it "knows the correct number of coins for the value" do
+        expect(coins.change_in_denomination(99, 25)).to eq(
+          { number_of_denomination: 3, remaining_change: 24 }
+        )
+      end
+    end
+  end
+
+  context "with an unsupported currency code" do
+    it "raised UnsupportedCurrencyError" do
+      expect {
+        Coins.new cents: 0, currency_code: 'FOO'
+      }.to raise_error(UnsupportedCurrencyError)
     end
   end
 end
